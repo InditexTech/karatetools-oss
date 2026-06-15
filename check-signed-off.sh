0bf5b50f785e607109ec0a325bc7b8c02eff2109
Teresa Masot <teresamib@ext.inditex.com>
2026-06-15 18:18:32 +0200

    [Karate-Tools] JDK 25 - Karate v1

--- check-signed-off.sh
+++
@@ -1,6 +0,0 @@
-
-git log 2b2310fef0c4fc70f9002433a1844e050f926830..HEAD \
-  --pretty=format:"%H" | while read c; do
-  git show -s --format=%B $c | grep -q "Signed-off-by" || \
-  git show -s --format="%h %s"
-done
